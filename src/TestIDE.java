854ac76d11cf7822dac99a01c15bb9b510d07c65
edem16 <te16.emil.edberg@umea.nti.se>
2017-11-06 10:47:24 +0100

    First commit

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
 public class TestIDE {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Skriv något");
-        String s = scanner.nextLine();
-        System.out.println("Du skrivde:" + s);
+        Scanner gissning = new Scanner(System.in);
+        double rng = Math.random();
+        int random = (int) (rng * 100) + 1;
+        int svar = 0;
+        while (true) {
+            System.out.println("gissa");
+            svar = gissning.nextInt();
+            if (svar > random) {
+                System.out.println("Talet ska vara mindre");
+            } else if(svar == random){
+                System.out.println("Du gissade rätt!");
+                System.exit(0);
+            }else if (svar < random){
+                System.out.println("talet ska vara större");
+        }
     }
-}
+}}
